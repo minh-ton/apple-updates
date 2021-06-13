806a240124aaf5af65feddf48849adad85476197
Minh Ton <37860569+Minh-Ton@users.noreply.github.com>
2021-06-13 14:53:13 +0700

    Added error handling to send to server functions

--- applesilicon/send.js
+++ applesilicon/send.js
@@ -1,5 +1,7 @@
 const firebase = require("firebase-admin");
 
+require('./error.js')();
+
 let db = firebase.firestore();
 
 const ios_database = db.collection('discord').doc('ios');
@@ -18,47 +20,47 @@ module.exports = function () {
             case "tvos":
                 const tvos = await tvos_database.get();
                 const tvos_guilds = tvos.data();
-                for (let channel in tvos.data()) if (global.bot.channels.cache.get(tvos_guilds[channel]) != undefined) global.bot.channels.cache.get(tvos_guilds[channel]).send(embed);
+                for (let channel in tvos.data()) if (global.bot.channels.cache.get(tvos_guilds[channel]) != undefined) global.bot.channels.cache.get(tvos_guilds[channel]).send(embed).catch(function (error) { send_error(error, "send.js", `send_tvos`, `send tvos update to channels`); });
                 break;
             case "audioos":
                 const audioos = await audioos_database.get();
                 const audioos_guilds = audioos.data();
-                for (let channel in audioos.data()) if (global.bot.channels.cache.get(audioos_guilds[channel]) != undefined) global.bot.channels.cache.get(audioos_guilds[channel]).send(embed);
+                for (let channel in audioos.data()) if (global.bot.channels.cache.get(audioos_guilds[channel]) != undefined) global.bot.channels.cache.get(audioos_guilds[channel]).send(embed).catch(function (error) { send_error(error, "send.js", `send_audioos`, `send audioos update to channels`); });
                 break;
             case "macos":
                 const macos = await macos_database.get();
                 const macos_guilds = macos.data();
-                for (let channel in macos.data()) if (global.bot.channels.cache.get(macos_guilds[channel]) != undefined) global.bot.channels.cache.get(macos_guilds[channel]).send(embed);
+                for (let channel in macos.data()) if (global.bot.channels.cache.get(macos_guilds[channel]) != undefined) global.bot.channels.cache.get(macos_guilds[channel]).send(embed).catch(function (error) { send_error(error, "send.js", `send_macos`, `send macos update to channels`); });
                 break;
             case "ios":
                 const ios = await ios_database.get();
                 const ios_guilds = ios.data();
-                for (let channel in ios.data()) if (global.bot.channels.cache.get(ios_guilds[channel]) != undefined) global.bot.channels.cache.get(ios_guilds[channel]).send(embed);
+                for (let channel in ios.data()) if (global.bot.channels.cache.get(ios_guilds[channel]) != undefined) global.bot.channels.cache.get(ios_guilds[channel]).send(embed).catch(function (error) { send_error(error, "send.js", `send_ios`, `send ios update to channels`); });
                 break;
             case "ipados":
                 const ipados = await ipados_database.get();
                 const ipados_guilds = ipados.data();
-                for (let channel in ipados.data()) if (global.bot.channels.cache.get(ipados_guilds[channel]) != undefined) global.bot.channels.cache.get(ipados_guilds[channel]).send(embed);
+                for (let channel in ipados.data()) if (global.bot.channels.cache.get(ipados_guilds[channel]) != undefined) global.bot.channels.cache.get(ipados_guilds[channel]).send(embed).catch(function (error) { send_error(error, "send.js", `send_ipados`, `send ipados update to channels`); });
                 break;
             case "watchos":
                 const watchos = await watchos_database.get();
                 const watchos_guilds = watchos.data();
-                for (let channel in watchos.data()) if (global.bot.channels.cache.get(watchos_guilds[channel]) != undefined) global.bot.channels.cache.get(watchos_guilds[channel]).send(embed);
+                for (let channel in watchos.data()) if (global.bot.channels.cache.get(watchos_guilds[channel]) != undefined) global.bot.channels.cache.get(watchos_guilds[channel]).send(embed).catch(function (error) { send_error(error, "send.js", `send_watchos`, `send watchos update to channels`); });
                 break;
             case "pkg":
                 const pkg = await pkg_database.get();
                 const pkg_guilds = pkg.data();
-                for (let channel in pkg.data()) if (global.bot.channels.cache.get(pkg_guilds[channel]) != undefined) global.bot.channels.cache.get(pkg_guilds[channel]).send(embed);
+                for (let channel in pkg.data()) if (global.bot.channels.cache.get(pkg_guilds[channel]) != undefined) global.bot.channels.cache.get(pkg_guilds[channel]).send(embed).catch(function (error) { send_error(error, "send.js", `send_pkg`, `send pkg link to channels`); });
                 break;
             case "delta":
                 const delta = await delta_database.get();
                 const delta_guilds = delta.data();
-                for (let channel in delta.data()) if (global.bot.channels.cache.get(delta_guilds[channel]) != undefined) global.bot.channels.cache.get(delta_guilds[channel]).send(embed);;
+                for (let channel in delta.data()) if (global.bot.channels.cache.get(delta_guilds[channel]) != undefined) global.bot.channels.cache.get(delta_guilds[channel]).send(embed).catch(function (error) { send_error(error, "send.js", `send_delta`, `send delta link to channels`); });
                 break;
             case "bot":
                 const bot = await bot_database.get();
                 const bot_guilds = bot.data();
-                for (let channel in bot.data()) if (global.bot.channels.cache.get(bot_guilds[channel]) != undefined) global.bot.channels.cache.get(bot_guilds[channel]).send(embed);;
+                for (let channel in bot.data()) if (global.bot.channels.cache.get(bot_guilds[channel]) != undefined) global.bot.channels.cache.get(bot_guilds[channel]).send(embed).catch(function (error) { send_error(error, "send.js", `send_bot`, `send bot update to channels`); });
                 break;
             default:
                 return;
--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 global.beta_release = false; // switch mode
-global.bot_version = "2.0.1";
-global.bot_updatedate = "June 12th, 2021"
+global.bot_version = "2.0.2";
+global.bot_updatedate = "June 13th, 2021"
 
 const Discord = require('discord.js');
 const Enmap = require("enmap");
