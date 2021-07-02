f790499b21257b63e0515cb7855b1d932a26e472
Minh Ton <37860569+Minh-Ton@users.noreply.github.com>
2021-07-02 15:14:52 +0700

    Added hedgefetch command
    Co-Authored-By: ASentientHedgehog <72932987+moosethegoose2213@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 global.beta_release = false; // switch mode
-global.bot_version = "2.1.0";
-global.bot_updatedate = "June 17th, 2021"
+global.bot_version = "2.1.1";
+global.bot_updatedate = "July 1st, 2021"
+global.script_path = process.cwd();
 
 const Discord = require('discord.js');
 const Enmap = require("enmap");
