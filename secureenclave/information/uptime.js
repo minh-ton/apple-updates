81c122b58d09f3526400f60b9903a1c3e4f384a2
Minh Ton <37860569+Minh-Ton@users.noreply.github.com>
2021-09-27 20:09:10 +0700

    Improve bot age

--- secureenclave/information/uptime.js
+++ secureenclave/information/uptime.js
@@ -12,7 +12,7 @@ module.exports = {
     async execute(message, args) {
         const embed = new Discord.MessageEmbed()
             .setColor("#228B22")
-            .setDescription(`**Bot Uptime: **${ms(global.bot.uptime, { verbose: true })}\n**Bot Age: **${ms(Math.abs(new Date() - new Date('2021/06/09 21:48:09')), { verbose: true })}`);
+            .setDescription(`**Bot Uptime: **${ms(global.bot.uptime, { verbose: true })}\n**Bot Age: **${ms(Math.abs(new Date() - new Date(global.bot.user.createdAt)), { verbose: true })}`);
         message.channel.send({ embeds: [embed] });
     },
 };
