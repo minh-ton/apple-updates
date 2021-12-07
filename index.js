c466258ab03542176613cc9e2a62e8c4f464cfb2
Minh Ton <37860569+Minh-Ton@users.noreply.github.com>
2021-12-07 20:30:15 +0700

    Update bot status

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ global.bot.on("ready", async () => {
     console.log(`Logged in as ${global.bot.user.tag}!`);
     console.log(`Currently in ${global.bot.guilds.cache.size} servers!`);
     console.log('Bot has started!');
-    global.bot.user.setActivity(`apple!help | Merry Christmas!`, { type: "PLAYING" });
+    global.bot.user.setActivity(`apple!help 🎄`, { type: "PLAYING" });
 });
 
 global.bot.commands = new Discord.Collection();
