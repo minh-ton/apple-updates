5678633b7837658fff8767c75276a505136630c5
Minh Ton <37860569+Minh-Ton@users.noreply.github.com>
2022-01-30 15:22:41 +0700

    Fix setFooter deprecation warnings

--- secureenclave/information/invite.js
+++ secureenclave/information/invite.js
@@ -14,7 +14,7 @@ module.exports = {
         let serverembed = new Discord.MessageEmbed()
             .setColor(randomColor())
             .setDescription(`<:discord:908005120783048744> Invite me to your servers by clicking the link below! \n> https://discordapp.com/oauth2/authorize?&client_id=852378577063116820&scope=bot+applications.commands&permissions=8`)
-            .setFooter(`Join our support server: https://discord.gg/ktHmcbpMNU`);
+            .setFooter({ text: "Join our support server: https://discord.gg/ktHmcbpMNU" });
         message.channel.send({ embeds: [serverembed] });
     },
 }
--- secureenclave/information/source.js
+++ secureenclave/information/source.js
@@ -15,7 +15,7 @@ module.exports = {
             .setColor(randomColor())
             .setDescription(`<:src:907994041428377620> Curious about how I work? Check out my source code on GitHub: https://github.com/Minh-Ton/apple-updates`)
             .setImage("https://opengraph.githubassets.com/36d2fab628fde8d4997fd9dc53b7106661678748a67e53d417040cfeb58f6148/Minh-Ton/apple-updates")
-            .setFooter(`Join our support server: https://discord.gg/ktHmcbpMNU`);
+            .setFooter({ text: "Join our support server: https://discord.gg/ktHmcbpMNU" });
         message.channel.send({ embeds: [embed] });
     },
 };
