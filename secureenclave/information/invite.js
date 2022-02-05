1e0b9b29c47d0ebf8f324a10de09f86e7e0ed095
Minh Ton <37860569+Minh-Ton@users.noreply.github.com>
2022-02-05 12:40:34 +0700

    Add notice to invite command

--- secureenclave/information/invite.js
+++ secureenclave/information/invite.js
@@ -23,9 +23,10 @@ module.exports = {
                     .setStyle('LINK'));
         let invite_embed = new Discord.MessageEmbed()
             .setColor(randomColor())
+            .setThumbnail(global.bot.user.displayAvatarURL({ format: "png", dynamic: true }))
             .setTitle(`${global.bot.user.tag} - Invite`)
             .setURL("https://discord.com/api/oauth2/authorize?client_id=852378577063116820&permissions=2416438352&scope=applications.commands%20bot")
-            .setDescription(`Click [here](https://discord.com/api/oauth2/authorize?client_id=852378577063116820&permissions=2416438352&scope=applications.commands%20bot) to invite the bot to your server.`)
+            .setDescription(`Click [here](https://discord.com/api/oauth2/authorize?client_id=852378577063116820&permissions=2416438352&scope=applications.commands%20bot) to invite the bot to your server.\n\nAfter inviting the bot to your server, make sure to run the \`setup\` command to configure it to your liking!`)
             .setFooter({ text: "Join our support server: https://discord.gg/ktHmcbpMNU" });
         await interaction.editReply({ embeds: [invite_embed], components: [button] });
     },
