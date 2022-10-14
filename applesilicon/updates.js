0378c663e57f7e9020a355172f2847ad4217233f
Minh Ton <37860569+Minh-Ton@users.noreply.github.com>
2022-10-14 17:12:28 +0700

    Added iOS 16

--- applesilicon/updates.js
+++ applesilicon/updates.js
@@ -19,18 +19,20 @@ module.exports = function () {
         if (macos) await fetch_macos_updates(audiences.macos_release, devices.macos.build, devices.macos.model, devices.macos.prodtype, devices.macos.version, 'public', false); // macOS Release
 
         // Beta iOS
-        if (ios) await fetch_other_updates(audiences.ios_15_beta, devices.ios.build, devices.ios.model, devices.ios.prodtype, devices.ios.version, "iOS", "beta", true); // iOS 15 Beta
+        if (ios) await fetch_other_updates(audiences.ios_15_beta, "19G71", "D101AP", "iPhone9,3", "15.6", "iOS", "beta", true); // iOS 15 Beta
         if (ios) await fetch_other_updates(audiences.ios_16_beta, devices.ios.build, devices.ios.model, devices.ios.prodtype, devices.ios.version, "iOS", "beta", true); // iOS 16 Beta
         // Public iOS
+        if (ios) await fetch_other_updates(audiences.ios_14_security, "18F72", "D101AP", "iPhone9,3", "14.6", "iOS", "public", false); // iOS 14
+        if (ios) await fetch_other_updates(audiences.ios_release, "19G71", "D101AP", "iPhone9,3", "15.6", "iOS", "public", false); // iOS 15
         if (ios) await fetch_other_updates(audiences.ios_release, devices.ios.build, devices.ios.model, devices.ios.prodtype, devices.ios.version, "iOS", "public", false); // iOS Release
-        if (ios) await fetch_other_updates(audiences.ios_14_security, "18F72", devices.ios.model, devices.ios.prodtype, "14.6", "iOS", "public", false); // iOS 14 Security Updates
 
         // Beta iPadOS
-        if (ipados) await fetch_other_updates(audiences.ios_15_beta, devices.ipados.build, devices.ipados.model, devices.ipados.prodtype, devices.ipados.version, "iPadOS", "beta", true); // iPadOS 15 Beta
+        if (ipados) await fetch_other_updates(audiences.ios_15_beta, "19G71", "J81AP", "iPad5,3", "15.6", "iPadOS", "beta", true); // iPadOS 15 Beta
         if (ipados) await fetch_other_updates(audiences.ios_16_beta, devices.ipados.build, devices.ipados.model, devices.ipados.prodtype, devices.ipados.version, "iPadOS", "beta", true); // iPadOS 16 Beta
         // Public iPadOS
+        if (ipados) await fetch_other_updates(audiences.ios_14_security, "18F72", "J81AP", "iPad5,3", "14.6", "iPadOS", "public", false); // iPadOS 14
+        if (ipados) await fetch_other_updates(audiences.ios_release, "19G71", "J81AP", "iPad5,3", "15.6", "iPadOS", "public", false); // iPadOS 15
         if (ipados) await fetch_other_updates(audiences.ios_release, devices.ipados.build, devices.ipados.model, devices.ipados.prodtype, devices.ipados.version, "iPadOS", "public", false); // iPadOS Release
-        if (ipados) await fetch_other_updates(audiences.ios_14_security, "18F72", devices.ipados.model, devices.ipados.prodtype, "14.6", "iPadOS", "public", false); // iPadOS 14 Security Updates
 
         // Beta watchOS
         if (watchos) await fetch_other_updates(audiences.watchos_8_beta, devices.watchos.build, devices.watchos.model, devices.watchos.prodtype, devices.watchos.version, "watchOS", "beta", true); // watchOS 8 Beta
