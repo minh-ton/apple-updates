bec1db428edb96c7df6f2064d061c47b710982e7
Minh Ton <37860569+Minh-Ton@users.noreply.github.com>
2023-06-05 10:13:39 +0700

    Clean up code

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 require('dotenv').config()
-require("./applesilicon/updates.js")();
-require("./applesilicon/embed.js")();
 
 global.BETA_RELEASE = false;
 global.UPDATE_MODE = false;
@@ -17,12 +15,18 @@ const firebase = require("firebase-admin");
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
+var firebase_token = (BETA_RELEASE) ? process.env.firebase_beta : process.env.firebase;
+var bot_token = (BETA_RELEASE) ? process.env.bot_beta_token : process.env.bot_token;
+
 firebase.initializeApp({
-    credential: firebase.credential.cert(JSON.parse(process.env.firebase))
+    credential: firebase.credential.cert(JSON.parse(firebase_token))
 });
 
+require("./applesilicon/updates.js")();
+require("./applesilicon/embed.js")();
+
 global.bot = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS], partials: [ 'CHANNEL' ] });
-global.bot.login(process.env.bot_token);
+global.bot.login(bot_token);
 
 // ============= DISCORD BOT ============
 
@@ -52,10 +56,10 @@ for (const category of commands) {
     }
 }
 
-const rest = new REST({ version: '9' }).setToken(process.env.bot_token);
+const rest = new REST({ version: '9' }).setToken(bot_token);
 (async() => {
     try { 
-        if (global.BETA_RELEASE) await rest.put(Routes.applicationGuildCommands(process.env.client_id, process.env.server_id), { body: command_collection });
+        if (global.BETA_RELEASE) await rest.put(Routes.applicationGuildCommands(process.env.beta_id, process.env.server_id), { body: command_collection });
         else await rest.put(Routes.applicationCommands(process.env.client_id), { body: command_collection });
     } catch (error) {
         console.error(error);
