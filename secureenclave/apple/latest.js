5190c14575b0bcced27323cb4102b59c0b57df15
Minh Ton <37860569+Minh-Ton@users.noreply.github.com>
2023-06-08 20:39:37 +0700

    Resolve a bug in the `latest` command

--- secureenclave/apple/latest.js
+++ secureenclave/apple/latest.js
@@ -40,16 +40,29 @@ module.exports = {
             const processing = new Discord.MessageEmbed().setColor(randomColor());
             await interaction.editReply({ embeds: [processing.setDescription("Hang on, I'm fetching data from Apple...")] });
 
-            let pkg_catalog = await get_pkg_assets(catalogs.macos_beta, 'beta_pkg');
+            let installers = get_links(await get_pkg_assets(catalogs.macos_beta, 'beta_pkg'));
+            let installers13 = get_links(await get_pkg_assets(catalogs.macos_beta_13, 'beta_pkg'));
+            let installers12 = get_links(await get_pkg_assets(catalogs.macos_beta_12, 'beta_pkg'));
+            let installers11 = get_links(await get_pkg_assets(catalogs.macos_beta_11, 'beta_pkg'));
 
-            let installers = get_links(pkg_catalog);
+            let installers_beta = `${installers11[0].sort().join('\n')}
+                                    ${installers12[0].sort().join('\n')}
+                                    ${installers13[0].sort().join('\n')}
+                                    ${installers[0].sort()}`;
+            
+            let embed_color = randomColor();
 
-            const embed = new Discord.MessageEmbed()
+            const public = new Discord.MessageEmbed()
                 .setTitle("macOS Full Installer Packages")
-                .setDescription(`**Public Release Installers:**\n${installers[1].join('\n')}\n\n**Beta Release Installers:**\n${installers[0].join('\n')}`)
-                .setColor(randomColor())
+                .setDescription(`**Public Release Installers:**\n${installers[1].sort().join('\n')}`)
+                .setColor(embed_color);
+
+            const beta = new Discord.MessageEmbed()
+                .setDescription(`**Beta Release Installers:**\n${installers_beta}`)
+                .setColor(embed_color)
                 .setFooter({ text: interaction.user.username, iconURL: interaction.user.displayAvatarURL() });
-            interaction.editReply({ embeds: [embed] });
+            
+            interaction.editReply({ embeds: [public, beta] });
         } catch (error) {
             return interaction.editReply(error_alert("Ugh, an unknown error occurred.", error));
         }
